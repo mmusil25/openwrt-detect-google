504ac7330b620972dc308225dc6d7a887315a0a4
Mark Musil <musilmark25@gmail.com>
2024-05-13 21:29:46 -0400

    add GPIO button input and triple LED output

--- detectGoogle.cpp
+++ detectGoogle.cpp
@@ -2,6 +2,63 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <gpiod.h>
+#include <iostream>
+
+using namespace std;
+
+void blinkThreeLEDs(int gpioPin, int Duration){
+
+    const char *chipname = "gpiochip0";
+    struct gpiod_chip *chip;
+    struct gpiod_line *lineRed;    // Red LED
+    struct gpiod_line *lineButton; // Pushbutton
+    struct gpiod_line *lineGreen;   // Green LED
+    struct gpiod_line *lineYellow; // yellow LED				    
+    int i, val; 
+
+
+    // Open GPIO chip
+    chip = gpiod_chip_open_by_name(chipname);
+
+    // Open GPIO lines
+    lineRed = gpiod_chip_get_line(chip,24);
+    lineGreen = gpiod_chip_get_line(chip,25);
+    lineYellow = gpiod_chip_get_line(chip,5);
+    lineButton = gpiod_chip_get_line(chip, 6);
+    // Open LED lines for output
+    gpiod_line_request_output(lineRed, "example1", 0);
+    gpiod_line_request_output(lineGreen, "example1", 0);
+    gpiod_line_request_output(lineYellow, "example1", 0);
+
+    // Open switch line for input
+    gpiod_line_request_input(lineButton, "example1");
+
+    // Blink LEDs in a binary pattern
+    i = 0;
+    while(true)
+    {
+	    gpiod_line_set_value(lineRed, (i & 1) != 0);
+	    gpiod_line_set_value(lineGreen, (i & 2) != 0);
+	    gpiod_line_set_value(lineGreen, (i & 4) != 0);
+
+	    // Read button status and exit if pressed
+	    val = gpiod_line_get_value(lineButton);
+	    if (val == 0){
+		break;
+    		}
+
+	    usleep(100000);
+	    i++;	    
+
+	gpiod_line_release(lineRed);
+	gpiod_line_release(lineGreen);
+	gpiod_line_release(lineYellow);
+	gpiod_line_release(lineButton);
+	gpiod_chip_close(chip);
+	return 0;
+
+}
 
 void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet) {
     const char *payload = (char *)(packet + 54); // Adjusted offset to skip Ethernet, IP, and TCP headers
@@ -9,6 +66,7 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_c
     fflush(stdout); // Flush the standard output buffer
 
     if (strstr(payload, "Host: www.google.com")) {
+	setGpioHighForDuration(gpioPin, duration);    
         printf("Google.com accessed\n");  // Signal detected access to Google
         fflush(stdout); // Flush the standard output buffer
     } else {
@@ -21,7 +79,7 @@ int main(int argc, char *argv[]) {
     if (argc < 3) {
         fprintf(stderr, "Usage: %s <device> <IP>\n", argv[0]);
         return 1;
-    }
+	    }
 
     char *device = argv[1];
     char *ip = argv[2];
